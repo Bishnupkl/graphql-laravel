c492ce25440c24c684d3931ef210449234603e3c
bishnu <bishnup212@gmail.com>
2025-02-17 21:55:21 -0600

    adding in to git first

---
+++ app/GraphQL/Queries/Greetings.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace App\GraphQL\Queries;
+
+final readonly class Greetings
+{
+    /** @param  array{}  $args */
+    public function __invoke(null $_, array $args)
+    {
+        return "Hello world";
+        // TODO implement the resolver
+    }
+}
